090bc187525c617e5720a40fd79476f9c7fd2556
chaitanya chorawala <chorawala.chaitany@hotmail.com>
2024-02-14 00:12:15 +0530

    root commit

--- src/app/Auth/Guards/admin-auth.guard.ts
+++
@@ -1,9 +0,0 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../../Services/auth.service';
-
-export const adminAuthGuard: CanActivateFn = (route, state) => {
-  const isLoggedIn = inject(AuthService).isAdmin();
-  if(!isLoggedIn) inject(Router).navigate(['/login']);
-  return isLoggedIn;
-};
